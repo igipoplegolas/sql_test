587d202fee11bb81dbcb15c8bb2991e605047d04
Daniel Igaz <igipoplegolas@gmail.com>
2013-01-06 13:40:34 +0100

    fixed column name from_date, added UNIQUE constraint for surrogate_key

--- init_type2.sql
+++ init_type2.sql
@@ -1,15 +1,17 @@
--- create dim_customers table
+-- init dim_customers table
 DROP TABLE IF EXISTS dim_customers;
 
 -- create sequence for dim_customers surrogate key
 DROP SEQUENCE IF EXISTS seq_surrogate_key;
 CREATE SEQUENCE seq_surrogate_key;
 
+-- create dim_customers table, every new record has its own
+-- surrogate key and timestampt of insertion
 CREATE TABLE dim_customers (
-	surrogate_key INTEGER PRIMARY KEY NOT NULL DEFAULT nextval('seq_surrogate_key'),
+	surrogate_key INTEGER PRIMARY KEY UNIQUE NOT NULL DEFAULT nextval('seq_surrogate_key'),
 	id INTEGER NOT NULL,
 	name VARCHAR,
 	address VARCHAR,
-	from_data DATE NOT NULL DEFAULT CURRENT_DATE,
+	from_date DATE NOT NULL DEFAULT CURRENT_DATE,
 	to_date DATE
 );
